7531a2b5f05f7d3ffa5aea762fab202eafb6dc95
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-06 15:00:00 +0000

    Add musicians unit test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
-// install dependencies
-const { execSync } = require('child_process');
-execSync('npm install');
-execSync('npm run seed');
+// Unnecessary commands:
+// const { execSync } = require('child_process');
+// execSync('npm install');
+// execSync('npm run seed');
 
 const request = require("supertest")
 const { db } = require('./db/connection');
@@ -9,14 +9,9 @@ const { Musician } = require('./models/index')
 const app = require('./src/app');
 const seedMusician = require("./seedData");
 
-
 describe('./musicians endpoint', () => {
-    // Write your tests here
-    
-    
-
-
-
-
-    
-})
+    test("to return expected status code", async () => {
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    });
+});
