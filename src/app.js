44c74d1e9bd9eb7a63cd71bfa2123bf241ceb532
CaydnPB <117025523+CaydnPB@users.noreply.github.com>
2023-11-07 15:41:27 +0000

    Add wildcard parameter to GET request

--- src/app.js
+++ src/app.js
@@ -3,45 +3,26 @@ const app = express();
 const { Band, Musician } = require("../models/index")
 const { db } = require("../db/connection")
 
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/musicians", async (req, res) => {
-    const findMusician = await Musician.findAll();
-    res.json(findMusician);
+    const everyMusician = await Musician.findAll();
+    res.json(everyMusician);
 });
-
-app.get("/musicians/1", async (req, res) => {
-    const findMusician = await Musician.findOne({ where: { id: 1 } });
+  
+app.get("/musicians/:id", async (req, res) => {
+    const findMusician = await Musician.findByPk(req.params.id);
     res.json(findMusician);
-});
-
-app.get("/musicians/2", async (req, res) => {
-    const findMusician = await Musician.findOne({ where: { id: 2 } });
-    res.json(findMusician);
-});
-
-app.get("/musicians/3", async (req, res) => {
-    const findMusician = await Musician.findOne({ where: { id: 3 } });
-    res.json(findMusician);
-});
+}); 
 
 app.get("/bands", async (req, res) => {
-    const findBand = await Band.findAll();
-    res.json(findBand);
+    const everyBand = await Band.findAll();
+    res.json(everyBand);
 });
 
-app.get("/bands/1", async (req, res) => {
-    const findBand = await Band.findOne({ where: { id: 1 } });
-    res.json(findBand);
-});
-
-app.get("/bands/2", async (req, res) => {
-    const findBand = await Band.findOne({ where: { id: 2 } });
-    res.json(findBand);
-});
-
-app.get("/bands/3", async (req, res) => {
-    const findBand = await Band.findOne({ where: { id: 3 } });
+app.get("/bands/:id", async (req, res) => {
+    const findBand = await Band.findByPk(req.params.id);
     res.json(findBand);
 });
 
